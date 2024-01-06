c758d039fc83e5df7cc3fe774116a7120df1a864
Jermaine Neal <2221629520@qq.com>
2024-01-06 23:19:27 +0800

    2319

--- ceshichajian20/__init__.py
+++ ceshichajian20/__init__.py
@@ -4,7 +4,6 @@ from aqt.utils import showInfo, qconnect
 from aqt.gui_hooks import main_window_did_init
 from datetime import datetime, timedelta
 import time
-import random
 
 def testFunction() -> None:
     # 获取到所有正在学习的卡片的id
